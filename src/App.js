64d52993a96c1d12975caf472af0e8524e25628c
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-09-29 03:43:22 +0600

    cart added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Exercise from './components/Exercise/Exercise';
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
+     <Exercise></Exercise>
      
     </div>
   );
---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import "./Exercise.css"
+
+const Exercise = () => {
+    const [carts,setCart]=useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setCart(data))
+
+    },[])
+    return (
+        <div className='exercise-container'>
+            <div className="cart-container">
+                {/* <h3>cart{cart.length}</h3> */}
+                {carts.map(cart=><Cart 
+                key={cart.id}
+                cart={cart}
+                ></Cart>)}
+            </div>
+            <div className="calculation-container"><h2>calculate</h2></div>     
+        </div>
+    );
+};
+
+export default Exercise;
