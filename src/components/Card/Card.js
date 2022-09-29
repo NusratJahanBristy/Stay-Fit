6d453581c6b066384b88b991f8624482a5ca2c0a
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-09-29 07:18:32 +0600

    cart section added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import "./Card.css"
 
-const Cart = (props) => {
-    const {handleAddClick,cart}=props;
+const Card = (props) => {
+    const {handleAddClick,carts}=props;
     const {name,img,time}=props.cart;
     
     return (
-        <div className='cart'>
+        <div className='card'>
             <img src={img} alt="img"></img>
-            <h3 className='cart-name'>{name}</h3>
+            <h3 className='card-name'>{name}</h3>
             <p>Time Required: {time}s</p>
-          <button onClick={()=>handleAddClick(cart)} className='btn-cart'>
+          <button onClick={()=>handleAddClick(carts)} className='btn-card'>
             <p>Add to Cart</p>
             
           </button>
@@ -18,4 +18,4 @@ const Cart = (props) => {
     );
 };
 
-export default Cart;
+export default Card;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import "./Cart.css"
+import img from "../../img/avater.png";
 
 const Cart = ({singleCalculate}) => {
     return (
-        <div>
-            <h2>singleCalculate</h2>
-                <p>select item{singleCalculate.length}</p>
+        <div className='cart'>
+            <div className='profile'>
+                <img src={img} alt="" />
+                <h3>Zahid Hossain</h3>
+            </div>
+            <div className='profile-info'>
+               <div>
+               <h4>75kg</h4>
+                <p>Weight</p>
+               </div>
+               <div>
+               <h4>6.5</h4>
+                <p>Height</p>
+               </div>
+               <div>
+               <h4>25yrs</h4>
+                <p>Age</p>
+               </div>
+            </div>
+            <h2>Add a Break</h2>
+            <div className='break-time'>
+                <p>10s</p>
+                <p>20s</p>
+                <p>30s</p>
+                <p>40s</p>
+            </div>
+
+
+<div className='time-show'>
+
+    <h2>Practice Details</h2>
+            <p>Practice time {singleCalculate.length}</p>
+            <p>Break time {singleCalculate.length}</p>
+</div>
+
+            <button className='btn-completed'>
+                <p>Activity Completed</p>
+            </button>
         </div>
     );
 };
